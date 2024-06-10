3c4383bf7810e1ea691b7504d3f7a9cf532459e1
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-06-10 19:57:47 +0530

    replacing apis

--- src/components/todoSlice.jsx
+++ src/components/todoSlice.jsx
@@ -2,22 +2,22 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
-    const response = await axios.get('http://localhost:5000/getTodos');
+    const response = await axios.get('https://mytodo-ass1.onrender.com/getTodos');
     return response.data;
 });
 
 export const addTodo = createAsyncThunk('todos/addTodo', async (todo) => {
-    const response = await axios.post('http://localhost:5000/setTodos', todo);
+    const response = await axios.post('https://mytodo-ass1.onrender.com/setTodos', todo);
     return response.data;
 });
 
 export const updateTodo = createAsyncThunk('todos/updateTodo', async (todo) => {
-    const response = await axios.put(`http://localhost:5000/updateTodos/${todo._id}`, todo);
+    const response = await axios.put(`https://mytodo-ass1.onrender.com/updateTodos/${todo._id}`, todo);
     return response.data;
 });
 
 export const deleteTodo = createAsyncThunk('todos/deleteTodo', async (id) => {
-    await axios.delete(`http://localhost:5000/deleteTodos/${id}`);
+    await axios.delete(`https://mytodo-ass1.onrender.com/deleteTodos/${id}`);
     return id;
 });
 
